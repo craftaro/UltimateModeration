2a213ca1a87cba1671f16d6e5a1256d5aa999854
Brianna <bri@songoda.com>
2019-04-04 13:41:07 -0400

    Fixed kick command.

--- src/main/java/com/songoda/ultimatemoderation/punish/Punishment.java
+++ src/main/java/com/songoda/ultimatemoderation/punish/Punishment.java
@@ -76,6 +76,12 @@ public class Punishment {
                 sendMessage(victim);
                 break;
             case KICK:
+                if (victim.isOnline()) {
+                    victim.getPlayer().kickPlayer(plugin.getLocale().getMessage("event.ban.message",
+                            reason == null ? "" : reason,
+                            Methods.makeReadable(duration)));
+                }
+                break;
             case WARNING:
                 sendMessage(victim);
                 break;
