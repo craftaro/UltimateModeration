cfeae089bf086b5bc3191e893a89956e5e2f6117
Brianna <bri@songoda.com>
2019-06-02 18:38:03 -0400

    Fixed a bunch of issues with the slowmode command.

--- src/main/java/com/songoda/ultimatemoderation/command/commands/CommandSlowMode.java
+++ src/main/java/com/songoda/ultimatemoderation/command/commands/CommandSlowMode.java
@@ -21,7 +21,8 @@ public class CommandSlowMode extends AbstractCommand {
     protected ReturnType runCommand(UltimateModeration instance, CommandSender sender, String... args) {
         if (args.length == 0) {
             ChatListener.setSlowModeOverride(0);
-            sender.sendMessage(instance.getLocale().getMessage("event.slowmode.disabled"));
+            sender.sendMessage(instance.getReferences().getPrefix() + instance.getLocale().getMessage("event.slowmode.disabled"));
+            return ReturnType.SUCCESS;
         } else if (args.length != 1)
             return ReturnType.SYNTAX_ERROR;
 
@@ -31,7 +32,7 @@ public class CommandSlowMode extends AbstractCommand {
 
         Bukkit.getOnlinePlayers().forEach(player ->
                 player.sendMessage(instance.getReferences().getPrefix() +
-                        instance.getLocale().getMessage("event.slowmode.enabled",(delay / 1000))));
+                        instance.getLocale().getMessage("event.slowmode.enabled", Methods.makeReadable(delay))));
 
         return ReturnType.SUCCESS;
     }
@@ -55,11 +56,11 @@ public class CommandSlowMode extends AbstractCommand {
 
     @Override
     public String getSyntax() {
-        return "/slowmode [delay]";
+        return "/slowmode [delay in seconds]";
     }
 
     @Override
     public String getDescription() {
-        return "Allows you to see inside of a players inventory.";
+        return "Allows you to slow down the chat. Accepts time in the format of 1s 5s 10s.";
     }
 }
